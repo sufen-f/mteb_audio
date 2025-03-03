56fb777992ba41f7adc7e0fd060856aac931981a
Ali Sartaz Khan <alisartazkhan@gmail.com>
2025-03-03 15:33:53 -0800

    update

--- run.py
+++ run.py
@@ -5,24 +5,24 @@ import itertools
 from tqdm import tqdm
 
 model_names = [
-    "facebook/wav2vec2-base",
-    "facebook/wav2vec2-base-960h",
-    "facebook/wav2vec2-large",
-    "facebook/wav2vec2-large-xlsr-53",
-    "facebook/wav2vec2-lv-60-espeak-cv-ft",
-    "microsoft/wavlm-large",
+    # "facebook/wav2vec2-base",
+    # "facebook/wav2vec2-base-960h",
+    # "facebook/wav2vec2-large",
+    # "facebook/wav2vec2-large-xlsr-53",
+    # "facebook/wav2vec2-lv-60-espeak-cv-ft",
     "microsoft/wavlm-base-plus-sd",
     "microsoft/wavlm-base-plus-sv",
     "microsoft/wavlm-base-sd",
     "microsoft/wavlm-base-sv",
     "microsoft/wavlm-base-plus",
     "microsoft/wavlm-base",
-    "openai/whisper-large-v3",
-    "openai/whisper-medium",
-    "openai/whisper-tiny",
-    "openai/whisper-base",
-    "openai/whisper-small",
-    "Qwen/Qwen2-Audio-7B"
+    "microsoft/wavlm-large",
+    # "openai/whisper-large-v3",
+    # "openai/whisper-medium",
+    # "openai/whisper-tiny",
+    # "openai/whisper-base",
+    # "openai/whisper-small",
+    # "Qwen/Qwen2-Audio-7B"
 ]
 
 
