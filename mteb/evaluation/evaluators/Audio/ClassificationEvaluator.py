6c1fc3bd4726aefd90ffb2256000397bf36d930a
sufen <sufenfong@gmail.com>
2025-03-05 20:42:15 -0800

    Added classification abstask, evaluator and voice gender, voice emotion tasks

--- mteb/evaluation/evaluators/Audio/ClassificationEvaluator.py
+++ mteb/evaluation/evaluators/Audio/ClassificationEvaluator.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 import math
 import os
+import random
 from typing import Any
 
 import numpy as np
@@ -261,7 +262,7 @@ class AudiologRegClassificationEvaluator(Evaluator):
         audio_column_name,
         label_column_name,
         task_name: str,
-        max_iter: int = 100,
+        max_iter: int = 400,
         encode_kwargs: dict[str, Any] = {},
         limit: int | None = None,
         **kwargs,
@@ -280,11 +281,13 @@ class AudiologRegClassificationEvaluator(Evaluator):
         self.y_train = dataset_train[label_column_name]
         self.dataset_test = dataset_test[audio_column_name]
         self.y_test = dataset_test[label_column_name]
+        self.labels = self.y_train + self.y_test
+
 
         self.max_iter = max_iter
         self.task_name = task_name
 
-    def __call__(self, model, test_cache=None):
+    def __call__(self, model, test_cache=None,encode_kwargs: dict[str, Any] = {}):
         scores = {}
         clf = LogisticRegression(
             random_state=self.seed,
@@ -293,22 +296,29 @@ class AudiologRegClassificationEvaluator(Evaluator):
             verbose=1 if logger.isEnabledFor(logging.DEBUG) else 0,
         )
 
-        X_train = model.get_audio_embeddings(
-            self.dataset_train, batch_size=self.encode_kwargs["batch_size"]
-        )
+        audio_embeddings = np.load(encode_kwargs["file_path"])
 
-        if test_cache is None:
-            X_test = model.get_audio_embeddings(
-                self.dataset_test, batch_size=self.encode_kwargs["batch_size"]
-            )
-            test_cache = X_test
-        else:
-            X_test = test_cache
+        random.seed(42)
+        combined = list(zip(audio_embeddings, self.labels))
+        random.shuffle(combined)
+        audio_embeddings, self.labels = map(list, zip(*combined))
+
+        if encode_kwargs["embed_limit"] is not None:
+            audio_embeddings = audio_embeddings[:encode_kwargs["embed_limit"]]
+            self.labels = self.labels[:encode_kwargs["embed_limit"]]
+        if encode_kwargs["test_split"] is not None:
+            datasize = len(audio_embeddings)
+            split_index = int(datasize * encode_kwargs["test_split"])
+            X_train = audio_embeddings[:split_index]
+            X_test = audio_embeddings[split_index:]
+            self.y_train = self.labels[:split_index]
+            self.y_test = self.labels[split_index:]
+
         logger.info("Fitting logistic regression classifier...")
-        if X_train.dtype == torch.bfloat16:
-            X_train = X_train.to(torch.float32)
-        if X_test.dtype == torch.bfloat16:
-            X_test = X_test.to(torch.float32)
+#        if X_train.dtype == torch.bfloat16:
+#            X_train = X_train.to(torch.float32)
+#        if X_test.dtype == torch.bfloat16:
+#            X_test = X_test.to(torch.float32)
         clf.fit(X_train, self.y_train)
         logger.info("Evaluating...")
         y_pred = clf.predict(X_test)
--- mteb/tasks/Audio/Classification/eng/VoiceGender.py
+++ mteb/tasks/Audio/Classification/eng/VoiceGender.py
@@ -16,12 +16,12 @@ class VoiceGenderClassification(AbsTaskAudioClassification):
         },
         type="AudioClassification", 
         category="a2a",
-        eval_splits=["train", "test"],  
+        eval_splits=["test"],
         eval_langs=["eng-Latn"],
-        main_score="accuracy",  
+        main_score="accuracy",
         date=("2024-01-01", "2024-12-31"),
         domains=["Spoken"],
-        task_subtypes=["Voice Gender Classification"],  
+        task_subtypes=["Voice Gender Classification"],
         license="not specified",
         annotations_creators="derived",
         dialect=[],
@@ -31,12 +31,20 @@ class VoiceGenderClassification(AbsTaskAudioClassification):
     def dataset_transform(self):
         ds_split = self.dataset["train"]
         audio = ds_split["audio"]
-        labels = ds_split["label"] 
+        labels = ds_split["label"]
+        datasize = len(ds_split)
+        split_index = int(datasize * .2)
+        audio_train = audio[:split_index]
+        audio_test = audio[split_index:]
+        labels_train = labels[:split_index]
+        labels_test = labels[split_index:]
+        '''
         audio = [{"array": item["array"], "sampling_rate": item["sampling_rate"]} for item in audio]
 
         audio_train, audio_test, labels_train, labels_test = train_test_split(
-            audio, labels, test_size=0.2, random_state=42, stratify=labels
+            audio, test_size=0.2, random_state=None, shuffle=False,
         )
+        '''
         self.dataset = datasets.DatasetDict({
             "train": datasets.Dataset.from_dict({"audio": audio_train, "label": labels_train}),
             "test": datasets.Dataset.from_dict({"audio": audio_test, "label": labels_test}),
@@ -48,15 +56,19 @@ if __name__ == "__main__":
     print(f"Loaded model type: {type(model)}")
     evaluation = mteb.MTEB(tasks=[VoiceGenderClassification()])
     classification_method = "logReg" #CHANGE TO K_NN IF NEEDED
-    encode_kwarg = {"hidden_layer": 6}
+    encode_kwarg = {"file_path": f"../../../../../embeddings_gender_cluster/{model_name}/0.5/embeddings.npy",
+                    "embed_limit": 512,
+                    "hidden_layer": 6,
+                    "test_split": .2,
+                    }
     dataset_size = 224
 
     results = evaluation.run(
         model,
-        output_folder=f"results_Emotions/{classification_method}/{dataset_size}/{model_name}",
+        output_folder=f"results_Gender/{classification_method}/{dataset_size}/{model_name}",
         overwrite_results=True,
-        classification_method=classification_method, 
-        limit=dataset_size,
+        classification_method=classification_method,
+        #limit=dataset_size,
         encode_kwargs=encode_kwarg
     )
     print(results)
