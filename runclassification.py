6f4f303b5a1f1a2bb9a12483903fa50de1d4d482
mn <mn@Ms-MacBook-Pro.local>
2025-03-07 17:55:26 -0800

    final run class and clust

--- runclassification.py
+++ runclassification.py
@@ -7,27 +7,25 @@ import multiprocessing
 import torch
 
 model_names = [
-    "facebook/wav2vec2-base",
-    # "facebook/wav2vec2-base-960h",
+    # "facebook/wav2vec2-base",
     # "facebook/wav2vec2-large",
     # "facebook/wav2vec2-large-xlsr-53",
     # "facebook/wav2vec2-lv-60-espeak-cv-ft",
-    # "microsoft/wavlm-base-plus-sd",
+    
     # "microsoft/wavlm-base-plus-sv",
-    # "microsoft/wavlm-base-sd",
     # "microsoft/wavlm-base-sv",
-    # "microsoft/wavlm-base-plus",
     # "microsoft/wavlm-base",
     # "microsoft/wavlm-large",
+    
     # "openai/whisper-large-v3",
-    # "openai/whisper-medium",
-    #"openai/whisper-tiny",
+    # "openai/whisper-tiny",
     # "openai/whisper-base",
     # "openai/whisper-small",
+    
     # "Qwen/Qwen2-Audio-7B"
 ]
 
-encode_hidden_layers = [0.5]
+encode_hidden_layers = [0.5,1]
 dataset_sizes = [1024]
 tasks = [[VoiceGenderClassification()],[CREMADEmotionClassification()]]
 tasks_name = ['gender','emotion']
@@ -46,7 +44,6 @@ for i in range(len(tasks)):
         for class_algo, hidden_layer_percentage, dataset_size in tqdm(
                 itertools.product(class_algos, encode_hidden_layers, dataset_sizes), 
                 total=len(class_algos)  * len(encode_hidden_layers) * len(dataset_sizes)):
-    
             
             print(f"results for Model={model_name}, Classification={class_algo}, Hidden Layer={hidden_layer_percentage}, Dataset Size={dataset_size}:")
             encode_kwarg = {"file_path": f"new_{task_name}_embeddings/{model_name}/{hidden_layer_percentage}/embeddings.npz", "embed_limit": dataset_size, "test_split": .8,} 
@@ -63,7 +60,6 @@ for i in range(len(tasks)):
                 print("ERROR")
                 continue
 
-            
             print(results)
         del model
         torch.cuda.empty_cache()
