7da3f857035bfdaca520403642fbd823c5d5ca26
sufen <sufenfong@gmail.com>
2025-04-06 19:14:34 -0700

    Revert "fixed image zeroshot classification naming issues"
    This reverts commit 21bcafe35903cf4e0b6c17aefc4fa42549aa8b29.

--- scripts/run.py
+++
@@ -1,9 +0,0 @@
-import mteb
-
-model_name = "microsoft/wavlm-base"
-model = mteb.get_model(
-    model_name, model_revision="efa81aae7ff777e464159e0f877d54eac5b84f81"
-)
-tasks = mteb.get_tasks(tasks=["VoiceGenderClustering"])
-evaluation = mteb.MTEB(tasks=tasks)
-results = evaluation.run(model, output_folder=f"results/{model_name}")
